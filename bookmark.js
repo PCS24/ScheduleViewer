ac5fd2bcafc1eff6ce27cacee4ef36d6a9c67ffe
MrNNP <3020493@edison.k12.nj.us>
2022-08-13 21:03:46 -0400

    make api endpoint based on current domain

--- bookmark.js
+++ bookmark.js
@@ -1,5 +1,12 @@
-javascript:
-console.log("Schedule Viewer Starting");
+javascript: console.log("Schedule Viewer Starting");
 var studentID = prompt("Enter Student ID: ");
-fetch(`https://parents.edison.k12.nj.us/genesis/parents?tab1=studentdata&tab2=studentsummary&action=ajaxGetBellScheduleForDate&studentid=${studentID}&scheduleDate=&schedView=&mpToView=1`)
-.then((res)=>res.text()).then((res) => {document.body.innerHTML = (res.replaceAll('\\', ''))}).then(() => {document.querySelector('div').remove();});
+fetch(
+  `https://${window.location.hostname}/genesis/parents?tab1=studentdata&tab2=studentsummary&action=ajaxGetBellScheduleForDate&studentid=${studentID}&scheduleDate=&schedView=&mpToView=1`
+)
+  .then((res) => res.text())
+  .then((res) => {
+    document.body.innerHTML = res.replaceAll("\\", "");
+  })
+  .then(() => {
+    document.querySelector("div").remove();
+  });
