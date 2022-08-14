e6515cbb6753f46ea21b395c195de257abe67c9d
MrNNP <3020493@edison.k12.nj.us>
2022-08-14 19:26:42 -0400

    include support for instances hosted on genesisedu domain

--- bookmark.js
+++ bookmark.js
@@ -1,1 +1,9 @@
-javascript: console.log("Schedule Viewer Starting");var studentID = prompt("Enter Student ID: ");fetch(`https://${window.location.hostname}/genesis/parents?tab1=studentdata&tab2=studentsummary&action=ajaxGetBellScheduleForDate&studentid=${studentID}&scheduleDate=&schedView=&mpToView=1`).then((res) => res.text()).then((res) => {document.body.innerHTML = res.replaceAll("\\", "");}).then(() => {document.querySelector("div").remove();});
+javascript: console.log("Schedule Viewer Starting");
+var studentID = prompt("Enter Student ID: ");
+if (!window.location.pathname.includes("/genesis/")) {
+    var school = window.location.pathname.split("/")[1];
+    fetch(`https://parents.genesisedu.com/${school}/parents?tab1=studentdata&tab2=studentsummary&action=ajaxGetBellScheduleForDate&studentid=${studentID}&scheduleDate=&schedView=&mpToView=1`).then((res) => res.text()).then((res) => { document.body.innerHTML = res.replaceAll("\\", ""); }).then(() => { document.querySelector("div").remove(); });
+
+} else {
+    fetch(`https://${window.location.hostname}/genesis/parents?tab1=studentdata&tab2=studentsummary&action=ajaxGetBellScheduleForDate&studentid=${studentID}&scheduleDate=&schedView=&mpToView=1`).then((res) => res.text()).then((res) => { document.body.innerHTML = res.replaceAll("\\", ""); }).then(() => { document.querySelector("div").remove(); });
+}
